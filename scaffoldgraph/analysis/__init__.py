bbe9f3389ca69df58ebc3c29e111779052246ddc
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2020-06-01 16:56:23 +0100

    added handling for verbosity levels < 0

--- scaffoldgraph/analysis/__init__.py
+++ scaffoldgraph/analysis/__init__.py
@@ -3,10 +3,11 @@ scaffoldgraph.analysis
 """
 
 from .representation import calc_average_pairwise_similarity, get_over_represented_scaffold_classes
-from .cse import CSE
+from .enrichment import calc_scaffold_enrichment, compound_set_enrichment
 
 __all__ = [
     'calc_average_pairwise_similarity',
     'get_over_represented_scaffold_classes',
-    'CSE',
+    'calc_scaffold_enrichment',
+    'compound_set_enrichment',
 ]
